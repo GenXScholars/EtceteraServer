2e0dde4df379558fbb061153310d3e01c0659cab
Ogaga <lordoadjaro@gmail.com>
2020-10-06 20:08:31 +0100

    fix: fixeed process env issues

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,7 +8,7 @@ module.exports = Object.freeze({
         name: 'ADJARO OGAGA',
         contact: 'ogagaadjaro@gmail.com'
     },
-    PORT :  process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : (process.env.NODE_ENV === 'test' ? 3030 : 4000),
+    PORT :  process.env.NODE_ENV === 'production' ? (process.env.PORT || 8080) : (process.env.NODE_ENV === 'test' ? 3030 : 4000),
      MONGO_URI : 'mongodb+srv://ogaga:1986LORDo@cluster0-yuz76.azure.mongodb.net/epayment?retryWrites=true&w=majority',
     // MONGO_URI : 'mongodb://localhost/epayment',
     MONGO_OPTIONS : {
